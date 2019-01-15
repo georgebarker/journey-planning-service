34e3f4d0c40cdc0223e727dc779e9cf7017f6d08
georgebarker <georgebarker1@me.com>
2019-01-15 17:57:46 +0000

    First commit

---
+++ src/main/java/com/georgebarker/journeyplanningservice/JourneyPlanningServiceApplication.java
@@ -0,0 +1,14 @@
+package com.georgebarker.journeyplanningservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JourneyPlanningServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JourneyPlanningServiceApplication.class, args);
+	}
+
+}
+
